cc4393c6def0285262cb2867c7c4e662f6e82aee
conor.ney <conor.ux@gmail.com>
2023-03-05 11:48:56 -0500

    init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/duexcoast/reverseproxy
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+)
+
+func main() {
+	// Define the backend server we proxy to
+	target, err := url.Parse("http://localhost:8000")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Stolen from `httputil.NewSingleHostReverseProxy()`
+	targetQuery := target.RawQuery
+	director := func(req *http.Request) {
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+		req.URL.Path, req.URL.RawPath = joinURLPath(target, req.URL)
+		if targetQuery == "" || req.URL.RawQuery == "" {
+			req.URL.RawQuery = targetQuery + req.URL.RawQuery
+		} else {
+			req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+		}
+		if _, ok := req.Header["User-Agent"]; !ok {
+			// explicitly disable User-Agent so it's not set to default value
+			req.Header.Set("User-Agent", "")
+		}
+	}
+
+	// Create the proxy object
+	proxy := &httputil.ReverseProxy{Director: director}
+
+	// Listen for http:// connections on port 80
+	srv := http.Server{Addr: ":80", Handler: proxy}
+
+	// Start the server
+	srv.ListenAndServe()
+
+}
+
+// helper functions
+
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
+}
+
+func joinURLPath(a, b *url.URL) (path, rawpath string) {
+	if a.RawPath == "" && b.RawPath == "" {
+		return singleJoiningSlash(a.Path, b.Path), ""
+	}
+	// Same as singleJoiningSlash, but uses EscapedPath to determine
+	// whether a slash should be added
+	apath := a.EscapedPath()
+	bpath := b.EscapedPath()
+
+	aslash := strings.HasSuffix(apath, "/")
+	bslash := strings.HasPrefix(bpath, "/")
+
+	switch {
+	case aslash && bslash:
+		return a.Path + b.Path[1:], apath + bpath[1:]
+	case !aslash && !bslash:
+		return a.Path + "/" + b.Path, apath + "/" + bpath
+	}
+	return a.Path + b.Path, apath + bpath
+}
